f7de316ee26436417e672fe23c0c42e3088b66c9
Aston Motes <astonm@gmail.com>
2013-06-09 18:38:20 -0700

    add examples for collection/heap vs. generic/heap.

---
+++ default.go
@@ -0,0 +1,39 @@
+// Example integer heap using the built-in container/heap based on example
+package main
+
+import "container/heap"
+import "fmt"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+        return x
+}
+
+func main() {
+	h := &IntHeap{}
+	heap.Init(h)
+	heap.Push(h, 2)
+	heap.Push(h, 5)
+	heap.Push(h, 3)
+	heap.Push(h, 1)
+	// heap.Push(h, "not a number") // this line will blow up at run time, but not compile time
+
+	heap.Remove(h, 1)
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
+	fmt.Println()
+}
---
+++ generic.go
@@ -0,0 +1,29 @@
+// Example integer heap using generic/heap
+package main
+
+import "github.com/astonm/generic/heap"
+import "fmt"
+
+type IntHeap struct {
+	heap.GenericHeap
+
+	Push func(int)
+	Pop  func() int
+	Remove func(int) int
+}
+func (*IntHeap) Less(a, b int) bool { return a < b}
+
+func main() {
+	h := &IntHeap{}
+	heap.Init(h)
+	h.Push(2)
+	h.Push(5)
+	h.Push(3)
+	h.Push(1)
+	//h.Push("not a number") // this line will blow up at compile time!
+	h.Remove(1)
+	for h.Len() > 0 {
+		fmt.Printf("%d ", h.Pop())
+	}
+	fmt.Println()
+}
